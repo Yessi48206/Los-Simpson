bcb41a3514906dfa125cbdd6ef349e8883aa9d0e
Yessica del carmen Juarez <Yessi48206@gmail.com>
2022-10-10 13:52:24 -0300

    visualizamos los datos en el componente

--- src/componente/Frases.jsx
+++ src/componente/Frases.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
 
-const frases = () => {
+const frases = ({personaje}) => {
   return (
-    <Container>
+    <Container className="my-5" >
       <Card className="bg-dark">
         <Row>
           <Col md={4}>
+            {/* 
+            
             <Card.Img
               variant="start"
               src="https://trello.com/1/cards/6328e276517f8e0208590f76/attachments/632b887c5d55fc04a2c53b56/previews/632b887d5d55fc04a2c53b5d/download/image.png"
               alt="logo homero"
               className="w-100"
             ></Card.Img>
+            */}
+            <img src={personaje.image} alt={personaje.character}></img>
           </Col>
           <Col md={8}>
             <Card.Body className="text-light">
-              <Card.Title>Homero Simpsons</Card.Title>
+              <Card.Title>{personaje.character} </Card.Title>
               <Card.Text className="my-3">
-                {" "}
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
-                sapiente id dignissimos delectus, iusto doloremque numquam sit
-                ipsum mollitia ad molestiae ratione enim nemo consequatur.
-                Maiores aut sed pariatur quod. Lorem ipsum dolor sit amet. Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-                sint.
+                {personaje.quote}
               </Card.Text>
             </Card.Body>
           </Col>
