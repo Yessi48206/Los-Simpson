0c06ab765e9fc230baad50fe2c13af80bed1727b
Yessica del carmen Juarez <Yessi48206@gmail.com>
2022-10-11 16:45:01 -0300

    puse el spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@
 import Frases from "./componente/Frases";
 import {Container, Button} from "react-bootstrap";
 import {useEffect, useState} from 'react';
-
+import Spinner from './componente/Spinner'
 function App() {
  const [personaje, setPersonaje] = useState({});
+ const [mostrarSpinner, setMostrarSpinner] = useState(true);
  //ciclo de vida
  useEffect(()=>{
   consultaAPI();
@@ -12,26 +13,38 @@ function App() {
  //realiza peticiones async=asincronica y se va atardar un tiempo en ejecutarse,  await=esperar, fetch=petisiones
  const consultaAPI=async()=>{
    try {
+    //actualizar el state del spinner
+    setMostrarSpinner(true)
     const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
     //con este metodo lo guardo en dato para mostrar
     const dato = await respuesta.json();
     console.log(dato[0]);
     setPersonaje(dato[0]);
+    setTimeout(() => {
+      setMostrarSpinner(false)
+    }, 2500);
+    // actualizar el state de spinner
+setMostrarSpinner(false);
     
   } catch (error) {
     console.log(error);
     //mostar un cartel al usuario
-    
+    setMostrarSpinner(false);
   }
  }
+ //mostrar operador ternario es similar al if ?si :no
+ //(condicion logica)? (codigo cuando la condicion es true) : (codigo cuando la condicion es falso)
+ const mostrarComponente= (mostrarSpinner === true)? (<div className="d-flex justify-content-center">
+ <Spinner></Spinner>
+ </div>) : (<Frases personaje={personaje} > </Frases>)
 
   return (
     < >
-    <Container className="my-5 text-center bg-danger">
+    <Container className="my-5 text-center ">
     <img src="https://trello.com/1/cards/6328e276517f8e0208590f76/attachments/632b8843a474c100700e1cfb/previews/632b8844a474c100700e1d03/download/image.png" alt="logo de los simpson" className="w-100" />
-<Button variant="warning" >Obtener frases</Button>
+<Button variant="warning" onClick={consultaAPI} >Obtener frases</Button>
     </Container>
-    <Frases personaje={personaje} > </Frases>
+    {mostrarComponente}
     </>
     
   );
